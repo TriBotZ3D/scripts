d033ad00fe09c414f9fa6bee01877c4881e0484c
TriBotZ3D <sockmonster@hotmail.co.uk>
2016-09-29 11:17:00 +0100

    Added combat sleeps

--- Utils/SleepUtils.java
+++ Utils/SleepUtils.java
@@ -28,6 +28,26 @@ public class SleepUtils {
         }, General.random(15000, 20000));
     }
 
+    public static void waitForIdle(int minTimeout, int maxTimeout){
+        Timing.waitCondition(new Condition() {
+            @Override
+            public boolean active() {
+                General.sleep(100, 300);
+                return Player.getAnimation() == -1;
+            }
+        }, General.random(minTimeout, maxTimeout));
+    }
+
+    public static void waitToLeaveCombat(){
+        Timing.waitCondition(new Condition() {
+            @Override
+            public boolean active() {
+                General.sleep(100, 300);
+                return Player.getRSPlayer().isInCombat();
+            }
+        }, General.random(30000, 60000));
+    }
+
     public static void waitForBankToOpen(){
         Timing.waitCondition(new Condition() {
             @Override
