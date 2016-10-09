3fd831ece0d56679b7247c54504094a7fb4df8bc
TriBotZ3D <sockmonster@hotmail.co.uk>
2016-10-09 15:46:43 +0100

    No changes

--- AccountStarter/AccountStarter.java
+++ AccountStarter/AccountStarter.java
@@ -1,17 +1,29 @@
 package scripts.AccountStarter;
 
+import org.tribot.api.input.Mouse;
+import org.tribot.api2007.Camera;
+import org.tribot.api2007.util.ThreadSettings;
 import org.tribot.script.Script;
 import org.tribot.script.ScriptManifest;
+import scripts.AccountStarter.Combat.ASCombat;
+import scripts.AccountStarter.Woodcutting.ASWoodcutting;
 
 /**
  * Created by James on 22/09/2016.
  */
-@ScriptManifest(authors = {"Z3D"}, name = "AIO Woodcutter", category = "Woodcutting")
+@ScriptManifest(authors = {"Z3D"}, name = "Account Starter", category = "Account Starter")
 public class AccountStarter extends Script {
 
 
     @Override
     public void run() {
 
+        Camera.setRotationMethod(Camera.ROTATION_METHOD.ONLY_MOUSE);
+        Mouse.setSpeed(81);
+        ThreadSettings.get().setClickingAPIUseDynamic(true);
+
+        while(true){
+            ASWoodcutting.main();
+    }
     }
 }
