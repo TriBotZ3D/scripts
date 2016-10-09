9f7022d005b8571f8a0e2770b897e445fdb91ddd
TriBotZ3D <sockmonster@hotmail.co.uk>
2016-10-09 15:51:04 +0100

    Finished Class

--- AccountStarter/Combat/ASCombat.java
+++ AccountStarter/Combat/ASCombat.java
@@ -12,7 +12,8 @@ import org.tribot.api2007.types.RSObject;
 import org.tribot.api2007.types.RSTile;
 import scripts.AccountStarter.Banking.ASBanking;
 import scripts.AccountStarter.Variables.ASAreas;
-import scripts.AccountStarter.Variables.ASNames;
+import scripts.AccountStarter.Variables.ASVariables;
+import scripts.AccountStarter.Variables.ASTiles;
 import scripts.Utils.InventoryUtils;
 import scripts.Utils.Misc;
 import scripts.Utils.SleepUtils;
@@ -23,7 +24,7 @@ import scripts.Utils.SleepUtils;
 public class ASCombat {
 
     private enum STATE {
-        STARTING, BANKING, FIGHTING, WALKING_TO_BANK, WALKING_TO_COMBAT_AREA, LOST, END
+        STARTING, BANKING, FIGHTING, WALKING_TO_BANK, WALKING_TO_COMBAT_AREA, LOST
     }
 
     private static STATE currentState = STATE.STARTING;
@@ -36,37 +37,33 @@ public class ASCombat {
         return STATE.LOST;
     }
 
-
     public static void main() {
 
-        switch (currentState) {
+    switch (currentState) {
 
-            case STARTING:
-                getInitialState();
-                break;
+        case STARTING:
+            currentState = getInitialState();
+            break;
 
-            case BANKING:
-                handleBank();
-                break;
+        case BANKING:
+            handleBank();
+            break;
 
-            case FIGHTING:
-                handleCombat();
-                break;
+        case FIGHTING:
+            handleCombat();
+            break;
 
-            case WALKING_TO_BANK:
-                walkToBank();
-                break;
+        case WALKING_TO_BANK:
+            walkToBank();
+            break;
 
-            case WALKING_TO_COMBAT_AREA:
-                walkToCombatArea();
-                break;
+        case WALKING_TO_COMBAT_AREA:
+            walkToCombatArea();
+            break;
 
-            case LOST:
-                handleLost();
-                break;
-
-            case END:
-                break;
+        case LOST:
+            handleLost();
+            break;
         }
     }
 
@@ -75,38 +72,87 @@ public class ASCombat {
         if (onlyCombatGearEquipped() && InventoryUtils.inventIsEmpty()) {
             currentState = STATE.WALKING_TO_COMBAT_AREA;
         } else {
-            if (gotAllCombatGear()) {
-                if (nonCombatGearItemsEquippedCount() == 0) {
-                    //If we are wearing anything, it is just combat gear
-                    if (allCombatGearIsEquipped()) {
-                        ASBanking.depositInventory();
-                    }else{
-                        equipGear();
+            if (InventoryUtils.itemEquippedOrInInvent(ASVariables.getSwordName())){
+                if (InventoryUtils.itemEquippedOrInInvent(ASVariables.getShieldName())){
+                    if (nonCombatGearItemsEquippedCount() == 0) {
+                        //If we are wearing anything, it is just combat gear
+                        if (allCombatGearIsEquipped()) {
+                            ASBanking.depositInventory();
+                        }else{
+                            equipGear();
+                        }
+                    } else {
+                        //Wearing stuff that's not needed for combat
+                        ASBanking.depositEquipment();
                     }
-                } else {
-                    //Wearing stuff that's not needed for combat
-                    ASBanking.depositEquipment();
+                }else{
+                    ASBanking.withdraw(ASVariables.getShieldName(), 1);
                 }
-            } else {
-                //Withdraw items
-                if (InventoryUtils.haveGotItem(ASNames.getSwordName()))
-                    ASBanking.withdraw(ASNames.getSwordName(), 1);
-                if (InventoryUtils.haveGotItem(ASNames.getShieldName()))
-                    ASBanking.withdraw(ASNames.getShieldName(), 1);
+            }else{
+                ASBanking.withdraw(ASVariables.getSwordName(), 1);
             }
         }
     }
 
-
     private static void handleCombat() {
 
         RSNPC[] targetNPC;
+
+            if (allCombatGearIsEquipped()) {
+                if (InventoryUtils.inventIsEmpty()) {
+                    if (ASAreas.getCombatArea().contains(Player.getPosition())) {
+                        if (!isInCombat()) {
+                            targetNPC = getTargetNPCs();
+                            if (targetNPC.length > 0) {
+                                if (targetNPC[0].isOnScreen()) {
+                                    if (targetNPC[0].click("Attack"))
+                                        SleepUtils.waitToEnterCombat();
+                                    General.sleep(500, 1500);
+                                } else {
+                                    Walking.blindWalkTo(targetNPC[0].getPosition());
+                                    SleepUtils.waitToStopWalking();
+                                }
+                            } else {
+                                if (!goblinHouseDoorIsOpen()) {
+                                    openGoblinHouseDoor();
+                                } else {
+                                    //Should rarely happen, so many fucking goblins!!
+                                    System.out.println("No " + ASVariables.getTargetNPCName() + "'s Found! Waiting for them to spawn.");
+                                    General.sleep(3000);
+                                }
+                            }
+                        } else {
+                            SleepUtils.waitToLeaveCombat();
+                        }
+                    } else {
+                        currentState = STATE.LOST;
+                    }
+                }else{
+                    Inventory.dropAllExcept(new String[] {ASVariables.getSwordName(), ASVariables.getShieldName()});
+                    SleepUtils.waitForEmptyInvent();
+                }
+            } else {
+                equipGear();
+            }
+
+    }
+
+    private static void handleLost() {
+        if (justGotCombatGear()) {
+            currentState = STATE.WALKING_TO_COMBAT_AREA;
+        } else {
+            currentState = STATE.WALKING_TO_BANK;
+        }
+    }
+
+
+    private static RSNPC[] getTargetNPCs(){
         if (ASAreas.getGoblinHouseArea().contains(Player.getPosition()) && !goblinHouseDoorIsOpen()) {
             //In goblin house and door is not open so only search for goblins in that area
-            targetNPC = NPCs.findNearest(new Filter<RSNPC>() {
+            return NPCs.findNearest(new Filter<RSNPC>() {
                 @Override
                 public boolean accept(RSNPC rsnpc) {
-                    if (rsnpc.getDefinition().getName().equals(ASNames.getTargetNPCName()) && !rsnpc.isInCombat()) {
+                    if (rsnpc.getDefinition().getName().equals(ASVariables.getTargetNPCName()) && !rsnpc.isInCombat()) {
                         if (ASAreas.getCombatArea().contains(rsnpc.getPosition()) && ASAreas.getGoblinHouseArea().contains(Player.getPosition())) {
                             return true;
                         }
@@ -116,11 +162,11 @@ public class ASCombat {
             });
         }else{
             //Either we're not in the house or the door is open, so search for goblins outside of the house
-            //Save dealing with the fucking akward door.
-            targetNPC = NPCs.findNearest(new Filter<RSNPC>() {
+            //Save dealing with the fucking awkward door.
+            return NPCs.findNearest(new Filter<RSNPC>() {
                 @Override
                 public boolean accept(RSNPC rsnpc) {
-                    if (rsnpc.getDefinition().getName().equals(ASNames.getTargetNPCName()) && !rsnpc.isInCombat()) {
+                    if (rsnpc.getDefinition().getName().equals(ASVariables.getTargetNPCName()) && !rsnpc.isInCombat()) {
                         if (ASAreas.getCombatArea().contains(rsnpc.getPosition()) && !ASAreas.getGoblinHouseArea().contains(rsnpc.getPosition())) {
                             return true;
                         }
@@ -129,139 +175,11 @@ public class ASCombat {
                 }
             });
         }
-
-        if (!Player.getRSPlayer().isInCombat()){
-            if (targetNPC.length > 0){
-                if (targetNPC[0].isOnScreen()){
-                    if (targetNPC[0].click("Attack")){
-                        //This may look weird but it's for those with high ping
-                        //When you click the npc it will run to them after a bit
-                        //The two sleeps prevent spam clicking when waiting for the player to move
-                        Timing.waitCondition(new Condition() {
-                            @Override
-                            public boolean active() {
-                                General.sleep(100, 300);
-                                return Player.getRSPlayer().isInCombat() || Player.isMoving();
-                            }
-                        }, General.random(7000, 12000));
-                        Timing.waitCondition(new Condition() {
-                            @Override
-                            public boolean active() {
-                                General.sleep(100, 300);
-                                return Player.getRSPlayer().isInCombat() || !Player.isMoving();
-                            }
-                        }, General.random(8000, 12000));
-                    }
-                }else{
-                    Walking.blindWalkTo(targetNPC[0].getPosition());
-                    SleepUtils.waitToStopWalking();
-                }
-            }else{
-                if (!goblinHouseDoorIsOpen()) {
-                    openGoblinHouseDoor();
-                }else {
-                    //Should rarely happen, so many fucking goblins!!
-                    System.out.println("No " + ASNames.getTargetNPCName() + "'s Found! Waiting for them to spawn.");
-                    General.sleep(3000);
-                }
-            }
-        }else{
-            SleepUtils.waitToLeaveCombat();
-        }
     }
 
-    private static void handleLost() {
-        if (justGotCombatGear()) {
-            walkToCombatArea();
-        } else {
-            walkToBank();
-        }
+    private static boolean isInCombat(){
+        return Player.getRSPlayer().getInteractingCharacter() != null;
     }
-
-
-    private static void equipGear() {
-        RSItem[] Sword = Inventory.find(ASNames.getSwordName());
-        RSItem[] Shield = Inventory.find(ASNames.getShieldName());
-
-        if (!Banking.isBankScreenOpen()) {
-            if (Sword.length > 0) {
-                if (Sword[0].click("Wield")) {
-                    Timing.waitCondition(new Condition() {
-                        @Override
-                        public boolean active() {
-                            General.sleep(100, 300);
-                            return Equipment.isEquipped(ASNames.getSwordName());
-                        }
-                    }, General.random(3000, 5000));
-                }
-            }
-            if (Shield.length > 0) {
-                if (Shield[0].click("Wear")) {
-                    Timing.waitCondition(new Condition() {
-                        @Override
-                        public boolean active() {
-                            General.sleep(100, 300);
-                            return Equipment.isEquipped(ASNames.getShieldName());
-                        }
-                    }, General.random(3000, 5000));
-                }
-            }
-        } else {
-            if (Banking.close())
-                SleepUtils.waitForBankToClose();
-        }
-
-    }
-
-    private static boolean justGotCombatGear() {
-        RSItem[] nonCmbInventItems = Inventory.find(new Filter<RSItem>() {
-            @Override
-            public boolean accept(RSItem rsItem) {
-                if (rsItem != null && rsItem.getDefinition() != null)
-                    return !rsItem.getDefinition().getName().equals(ASNames.getSwordName()) && !rsItem.getDefinition().getName().equals(ASNames.getShieldName());
-
-                return false;
-            }
-        });
-
-        if (nonCmbInventItems.length > 0)
-            return false;
-
-        RSItem[] nonCmbEquippedItems = Inventory.find(new Filter<RSItem>() {
-            @Override
-            public boolean accept(RSItem rsItem) {
-                if (rsItem != null && rsItem.getDefinition() != null)
-                    return !rsItem.getDefinition().getName().equals(ASNames.getSwordName()) && !rsItem.getDefinition().getName().equals(ASNames.getShieldName());
-
-                return false;
-            }
-        });
-
-        return nonCmbEquippedItems.length == 0;
-
-    }
-
-    private static boolean gotAllCombatGear() {
-        return InventoryUtils.haveGotItem(ASNames.getSwordName()) && InventoryUtils.haveGotItem(ASNames.getShieldName());
-    }
-
-    private static boolean allCombatGearIsEquipped() {
-        return Equipment.isEquipped(ASNames.getSwordName()) && Equipment.isEquipped(ASNames.getShieldName());
-    }
-
-    private static boolean onlyCombatGearEquipped() {
-        return allCombatGearIsEquipped() && Equipment.getItems().length == 2;
-    }
-
-    private static int nonCombatGearItemsEquippedCount() {
-        return Equipment.find(new Filter<RSItem>() {
-            @Override
-            public boolean accept(RSItem rsItem) {
-                return !rsItem.getDefinition().getName().equals(ASNames.getSwordName()) && !rsItem.getDefinition().getName().equals(ASNames.getShieldName());
-            }
-        }).length;
-    }
-
 
     private static void openGoblinHouseDoor() {
         RSObject[] Door = Objects.findNearest(10, new Filter<RSObject>() {
@@ -297,17 +215,98 @@ public class ASCombat {
     }
 
 
+    private static void equipGear() {
+        RSItem[] Sword = Inventory.find(ASVariables.getSwordName());
+        RSItem[] Shield = Inventory.find(ASVariables.getShieldName());
+
+        if (!Banking.isBankScreenOpen()) {
+            if (Sword.length > 0) {
+                if (Sword[0].click("Wield")) {
+                    Timing.waitCondition(new Condition() {
+                        @Override
+                        public boolean active() {
+                            General.sleep(100, 300);
+                            return Equipment.isEquipped(ASVariables.getSwordName());
+                        }
+                    }, General.random(3000, 5000));
+                }
+            }
+            if (Shield.length > 0) {
+                if (Shield[0].click("Wield")) {
+                    Timing.waitCondition(new Condition() {
+                        @Override
+                        public boolean active() {
+                            General.sleep(100, 300);
+                            return Equipment.isEquipped(ASVariables.getShieldName());
+                        }
+                    }, General.random(3000, 5000));
+                }
+            }
+        } else {
+            if (Banking.close())
+                SleepUtils.waitForBankToClose();
+        }
+
+    }
+
+    private static boolean justGotCombatGear() {
+
+        if (!gotAllCombatGear())
+            return false;
+
+        RSItem[] nonCmbInventItems = Inventory.find(new Filter<RSItem>() {
+            @Override
+            public boolean accept(RSItem rsItem) {
+                return !rsItem.getDefinition().getName().equals(ASVariables.getSwordName()) && !rsItem.getDefinition().getName().equals(ASVariables.getShieldName());
+
+            }
+        });
+
+        if (nonCmbInventItems.length > 0)
+            return false;
+
+        RSItem[] nonCmbEquippedItems = Inventory.find(new Filter<RSItem>() {
+            @Override
+            public boolean accept(RSItem rsItem) {
+                return !rsItem.getDefinition().getName().equals(ASVariables.getSwordName()) && !rsItem.getDefinition().getName().equals(ASVariables.getShieldName());
+
+            }
+        });
+
+        if (nonCmbEquippedItems.length > 0)
+            return false;
+
+        return true;
+
+    }
+
+    private static boolean gotAllCombatGear() {
+        return InventoryUtils.itemEquippedOrInInvent(ASVariables.getSwordName()) && InventoryUtils.itemEquippedOrInInvent(ASVariables.getShieldName());
+    }
+
+    private static boolean allCombatGearIsEquipped() {
+        return Equipment.isEquipped(ASVariables.getSwordName()) && Equipment.isEquipped(ASVariables.getShieldName());
+    }
+
+    private static boolean onlyCombatGearEquipped() {
+        return allCombatGearIsEquipped() && Equipment.getItems().length == 2;
+    }
+
+    private static int nonCombatGearItemsEquippedCount() {
+        return Equipment.find(new Filter<RSItem>() {
+            @Override
+            public boolean accept(RSItem rsItem) {
+                return !rsItem.getDefinition().getName().equals(ASVariables.getSwordName()) && !rsItem.getDefinition().getName().equals(ASVariables.getShieldName());
+            }
+        }).length;
+    }
+
+
     private static void walkToCombatArea() {
         if (!ASAreas.getCombatArea().contains(Player.getPosition())) {
             if (Player.getPosition().getPlane() == 0) {
-                WebWalking.walkTo(Misc.getCentreTile(ASAreas.getCombatArea()));
-                Timing.waitCondition(new Condition() {
-                    @Override
-                    public boolean active() {
-                        General.sleep(200, 800);
-                        return ASAreas.getCombatArea().contains(Player.getPosition());
-                    }
-                }, General.random(5000, 8000));
+                WebWalking.walkTo(ASTiles.getCentreCombatTile());
+                SleepUtils.waitToStopWalking();
             } else {
                 ASBanking.leaveBank();
             }
