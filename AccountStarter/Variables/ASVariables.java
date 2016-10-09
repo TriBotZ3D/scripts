fb2c4db462341ee15d59a249057c96fe3a1ba50a
TriBotZ3D <sockmonster@hotmail.co.uk>
2016-10-09 17:00:45 +0100

    -

---
+++ AccountStarter/Variables/ASVariables.java
@@ -0,0 +1,32 @@
+package scripts.AccountStarter.Variables;
+
+/**
+ * Created by James on 29/09/2016.
+ */
+public class ASVariables {
+
+    //#IDs#
+
+    private static int deathAnimID = 0;
+    public static int getDeathAnimID(){return deathAnimID;}
+
+    private static int[] rockIDs = {0};
+    public static int[] getRockIDs(){return rockIDs;}
+
+    //#Names#
+
+    private static String axeName = "Bronze axe";
+    public static String getAxeName(){return axeName;}
+
+    private static String swordName = "Bronze sword";
+    public static String getSwordName(){return swordName;}
+
+    private static String shieldName = "Wooden shield";
+    public static String getShieldName(){return shieldName;}
+
+    private static String targetNPCName = "Goblin";
+    public static String getTargetNPCName(){return targetNPCName;}
+
+
+
+}
